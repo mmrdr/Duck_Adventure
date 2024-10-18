3d659536d3f780f896ae9eac290cca912ce93e45
mmrdr <kialisaev@edu.hse.ru>
2024-10-18 18:54:20 +0300

    Fixed name of player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] GameInput gameInput;
     [SerializeField] float moveSpeed = 7f;
