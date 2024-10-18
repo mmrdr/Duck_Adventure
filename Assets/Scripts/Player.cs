4be4c580620e03c8cfdc9a8b8f118145ec492b0e
mmrdr <kialisaev@edu.hse.ru>
2024-10-19 00:20:22 +0300

    Created collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -12,8 +13,36 @@ public class Player : MonoBehaviour
     {
         Vector2 input = gameInput.GetInputVectorNormalized();
         Vector3 moveDirection = new Vector3(input.x, 0f, input.y);
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
-
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerHeight = 1f;
+        float playerRadius = 0.25f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveX, moveDistance);
+            if (canMove)
+            {
+                moveDirection = moveX;
+            }
+            else
+            {
+                Vector3 moveZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveZ, moveDistance);
+                if (canMove)
+                {
+                    moveDirection = moveZ;
+                }
+                else
+                {
+                    Debug.Log("Застряли");
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotationSpeed * Time.deltaTime);
     }
